67b800d7280ca62f8263ffef7be694a8e2b1e994
victor-Silvadev <notebookconta037@gmail.com>
2026-04-09 20:04:31 -0300

    aula de condiçoes

---
+++ aula12/ex02.js
@@ -0,0 +1,31 @@
+/* condiçao multiplas */
+
+
+let agora = new Date()
+let diaSem = agora.getDay()
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+    case 3:
+        break
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+    default :
+        console.log('Dia invalido')
+}
---
+++ aula12/java.js
@@ -0,0 +1,11 @@
+let time = 'flamengo'
+
+if (time == 'flamengo') {
+    console.log('Voce e flamenguista')
+} 
+    else if(time == 'fluminense') {
+    console.log('Voce e tricolo')
+} 
+    else {
+    console.log('voce e gay entao')
+}
