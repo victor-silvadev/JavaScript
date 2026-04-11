35dfb9124fcb7a5dc4635169ace763389e90d84d
victor-Silvadev <notebookconta037@gmail.com>
2026-04-11 03:06:46 -0300

    exercicio de indentificador de idades

--- exercicios/ex02/script2.js
+++ exercicios/ex02/script2.js
@@ -12,14 +12,50 @@ function verificar() {
         let sexo = document.getElementsByName('sexo')
         let idade = ano - Number(fAno.value)
         let genero = '' 
+
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if(sexo[0].checked) {
         genero = 'Homem' 
+        if (idade >= 0 && idade < 4) {
+            /*  BEBE  */
+            img.setAttribute('src', 'bebe-menino.jpg')
+        } else if (idade < 15) {
+            /* CRIANÇA */
+            img.setAttribute('src', 'criança-M.jpg')
+        } else if (idade < 21) {
+            /* JOVEM */
+            img.setAttribute('src', 'jovem-M.jpg')
+        } else if (idade < 60) {
+            /* ADULTO */
+            img.setAttribute('src', 'adulto-M.jpg')
+        } else {
+            /* VELHO */
+            img.setAttribute('src', 'velho-M.jpg')
+        }
     } 
     else if (sexo[1].checked) {
             genero = 'Mulher'
-    }
-    reS.style.textAlling = 'center'
+                    if (idade >= 0 && idade < 4) {
+            /*  BEBE  */
+            img.setAttribute('src', 'bebe-menina.jpg')
+        } else if (idade < 15) {
+            /* CRIANÇA */
+            img.setAttribute('src', 'criança-F.jpg')
+        } else if (idade < 21) {
+            /* JOVEM */
+            img.setAttribute('src', 'jovem-F.jpg')
+        } else if (idade < 60) {
+            /* ADULTO */
+            img.setAttribute('src', 'adulto-F.jpg')
+        } else {
+            /* VELHO */
+            img.setAttribute('src', 'velho-F.jpg')
+        }
+    } 
+    reS.style.textAlign = 'center'
     reS.innerHTML = `Detectamos ${genero} com ${idade} anos.`
-}
-
+    reS.appendChild(img)
+    }
 }
