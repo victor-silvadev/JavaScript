8c4ed8585b5293ab82f0cc86ff85739c53d9b2e0
victor-Silvadev <notebookconta037@gmail.com>
2026-04-11 21:30:12 -0300

    exercicio de repetiçao

---
+++ aula13/node_1f8c7ad847a7a.tmp.js
@@ -0,0 +1,4 @@
+let c = 1 
+ while (c <= 6) {
+    console.log('Tudo Bem?')
+ }
---
+++ aula13/teste.js
@@ -0,0 +1,21 @@
+let c = 2
+do {
+   console.log(`passo ${c}`)
+      c++
+}   while (c <= 10) {
+   console.log('Chegou')
+}
+
+
+
+
+
+
+
+
+ /* let c = 1 
+ while (c <= 10) {
+    console.log(`passo ${c}`)
+    c++
+ }  
+*/
